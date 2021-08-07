b98fae5bce74d10dbe64a8956e6f8d3e4de5cd9c
“杜” <1050466228@qq.com>
2021-08-07 14:41:11 +0800

    2

---
+++ src/main/java/com/example/test2021/Student.java
@@ -0,0 +1,11 @@
+package com.example.test2021;
+
+/**
+ * @author DnJuu
+ * @Package com.example.test2021
+ * @date 2021/8/7 14:39
+ */
+public class Student {
+    private String name;
+    private Integer age;
+}
